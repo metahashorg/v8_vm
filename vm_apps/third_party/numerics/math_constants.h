a7744026583e38d74b99b2148d9bc8eb5ca14e79
Denis Gushchin <dsntfd@gmail.com>
2018-12-19 17:19:50 +0300

    Add a numerics library by Chromium Authors

---
+++ vm_apps/third_party/numerics/math_constants.h
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_NUMERICS_MATH_CONSTANTS_H_
+#define BASE_NUMERICS_MATH_CONSTANTS_H_
+
+constexpr double kPiDouble = 3.14159265358979323846;
+constexpr float kPiFloat = 3.14159265358979323846f;
+
+#endif  // BASE_NUMERICS_MATH_CONSTANTS_H_
